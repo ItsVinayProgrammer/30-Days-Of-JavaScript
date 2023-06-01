1049544e82a10afb15ebf1a2aa8745eb1f395938
Vinay Verma <131639993+ItsVinayProgrammer@users.noreply.github.com>
2023-06-01 18:29:45 +0530

    Add files via upload

---
+++ Day 1_Variables _and_Data_Types.js
@@ -0,0 +1,17 @@
+// Day 1: Variables and Data Types
+
+// Declare a variable and assign a value to it
+let name = "V.Vinay Verma";
+
+// Print the value of the variable
+console.log("My name is " + name);
+
+// Declare and initialize variables of different data types
+let age = 18;
+let isStudent = true;
+let height = 195.7;
+
+// Print the values and types of the variables
+console.log("Age: " + age + ", Type: " + typeof age);
+console.log("Is Student: " + isStudent + ", Type: " + typeof isStudent);
+console.log("Height: " + height + ", Type: " + typeof height);
