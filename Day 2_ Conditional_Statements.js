60723801372639de475fab13e2e7689b2a0a69d9
Vinay Verma <131639993+ItsVinayProgrammer@users.noreply.github.com>
2023-06-02 08:44:28 +0530

    Add files via upload

---
+++ Day 2_ Conditional_Statements.js
@@ -0,0 +1,22 @@
+// Day 2: Conditional Statements
+
+// Declare a variable for age
+let age = 18;
+
+// Check if the person is eligible to vote
+if (age >= 18) {
+  console.log("You are eligible to vote.");
+} else {
+  console.log("You are not eligible to vote yet.");
+}
+
+// Check if a number is positive, negative, or zero
+let number = -5;
+
+if (number > 0) {
+  console.log("The number is positive.");
+} else if (number < 0) {
+  console.log("The number is negative.");
+} else {
+  console.log("The number is zero.");
+}
