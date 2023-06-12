f08e13dde27e583a28da73033326069c16fcdd06
Vinay Verma <131639993+ItsVinayProgrammer@users.noreply.github.com>
2023-06-12 21:37:48 +0530

    Add files via upload

---
+++ Day 12_Math_Operations.js
@@ -0,0 +1,34 @@
+// Day 12: Math Operations
+
+// Example of basic math operations
+let x = 5;
+let y = 3;
+
+let addition = x + y;
+let subtraction = x - y;
+let multiplication = x * y;
+let division = x / y;
+let modulus = x % y;
+let exponentiation = x ** y;
+
+console.log("Addition:", addition);
+console.log("Subtraction:", subtraction);
+console.log("Multiplication:", multiplication);
+console.log("Division:", division);
+console.log("Modulus:", modulus);
+console.log("Exponentiation:", exponentiation);
+
+// Example of Math object functions
+let number = -4.6;
+
+let absoluteValue = Math.abs(number);
+let roundedValue = Math.round(number);
+let ceilingValue = Math.ceil(number);
+let floorValue = Math.floor(number);
+let squareRoot = Math.sqrt(x);
+
+console.log("Absolute Value:", absoluteValue);
+console.log("Rounded Value:", roundedValue);
+console.log("Ceiling Value:", ceilingValue);
+console.log("Floor Value:", floorValue);
+console.log("Square Root:", squareRoot);
